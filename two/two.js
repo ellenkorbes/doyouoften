71de7cd72199985d4af5a4cbcc47031b3d8a5da8
Ellen Körbes <ellenkorbes@gmail.com>
2019-11-11 18:55:59 +0100

    Update two.js

--- two/two.js
+++ two/two.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-	response.end('Hello from service One!');
+	response.end('Hello from service Two!');
 });
 
 server.listen(8080, () => console.log(`Server is listening!`));
